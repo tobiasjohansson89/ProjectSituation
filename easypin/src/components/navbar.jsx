8d9f529635967c7be99d71d8cd9fe571e8b30e2c
Trine Marie Sandberg <trine.marie.sandberg@gmail.com>
2023-04-07 13:59:15 +0200

    ads header and navbar

---
+++ easypin/src/components/navbar.jsx
@@ -0,0 +1,18 @@
+export default function Navbar() {
+    return (
+    <>
+    <h2>Navbar</h2>
+    <nav>
+        <ul>
+            <li>
+                <div className="navbar">
+                    <p>Home</p>
+                    <p>About</p>
+                    <button>Login</button>
+                </div>          
+            </li>
+        </ul>
+    </nav>
+    </>
+    );
+    }
