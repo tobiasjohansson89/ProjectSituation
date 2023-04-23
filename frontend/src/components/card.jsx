76ab547dd564e9d376162208ec991b8f592d0a9e
Trine Marie Sandberg <trine.marie.sandberg@gmail.com>
2023-04-23 20:20:21 +0200

    ads apicall

--- frontend/src/components/card.jsx
+++ frontend/src/components/card.jsx
@@ -1,10 +1,34 @@
+import { useState, useEffect } from 'react';
+
 export default function Card() {
-    return (
-    <>
-    <div className="card">
-        <h2>TaskName</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid ut, reprehenderit dolor illum maxime dolore sed inventore itaque sunt assumenda.</p>
-    </div>
-    </>
-    );
+    
+    const [data, setData] = useState([]);
+    const urlMongoDb = "http://10.0.0.68:5000/tasks";
+    const dummyApi = "https://dummyjson.com/products";
+
+    const getData = async () => {
+        const response = await fetch(urlMongoDb);
+        setData(await response.json());
+    }
+
+    useEffect(() => {
+        getData()
+    }, []);
+
+    let title;
+    let description;
+    for(let i = 0; i < data.length; i++) {
+        title = data[i].title;
+        description = data[i].description
+    }
+
+    console.log(data)
+            return (
+            <>
+            <div className="card">
+                <h2>{title}</h2>
+                <p>{description}</p>
+            </div>
+            </>
+            );
     }
