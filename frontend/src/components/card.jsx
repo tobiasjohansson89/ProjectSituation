4b9ff8f92ff05abfea579f1d6a3404d71ebb2300
Trine Marie Sandberg <trine.marie.sandberg@gmail.com>
2023-04-26 20:52:47 +0200

    fixes isue with key/unique id

--- frontend/src/components/card.jsx
+++ frontend/src/components/card.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-
+let ids = [];
+let id;
 export default function Card() {
     
     const [data, setData] = useState([]);
@@ -11,22 +12,23 @@ export default function Card() {
         const response = await fetch(urlMongoDb);
         setData(await response.json());
     }
-
+    
     useEffect(() => {
         getData()
     }, []);
 
 return (<div className='cards-container'>
     {data.map((data) => { 
-                return (<Link to="DetailsPage">
-                          <div className="card"key={data._id.$oid}>
+                return (
+                <Link to="DetailsPage" key={data._id.$oid}>
+                          <div className="card" >
                              <h2>{data.title}</h2>
                              <p>{data.description}</p>
                              <p>{data.category}</p>
                              <p>{data.deadline}</p>
                              <p>{data.author}</p>
                           </div>
-                        </Link>)
+                </Link>)
                         })
     }
        </div>)
