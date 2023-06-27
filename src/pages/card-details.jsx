ec2d3c4b48543de7a1f5216bdb7a5c4d83fee750
Trine Marie Sandberg <trine.marie.sandberg@gmail.com>
2023-06-27 15:59:00 +0200

    adds deleteCard function

--- src/pages/card-details.jsx
+++ src/pages/card-details.jsx
@@ -2,62 +2,96 @@ import Header from "../components/header";
 import { TaskForm } from "../components/task-form";
 import { Link } from "react-router-dom";
 import { useState, useEffect } from 'react';
-let task;
-
-function getSingleCard() {
-const queryString = document.location.search;
-const param = new URLSearchParams(queryString);
-const id = param.get("id")
-    
-const [data, setData] = useState([]);
-const urlMongoDb = `http://10.0.0.68:5000/task/${id}`;
-
-const getCard = async () => {
-    const response = await fetch(urlMongoDb);
-    setData(await response.json());
-}
-
-useEffect(() => {
-    getCard()
-}, []);
-
-task = data;
-//console.log(task)
-}
-const deleteTask = async () => {
-    
-    const queryString = document.location.search;
-    const param = new URLSearchParams(queryString);
-    const id = param.get("id")
-    
-
-    const urlMongoDbdelete = `http://10.0.0.68:5000/delete/${id}`;
-    const response = await fetch(urlMongoDbdelete, {method: 'DELETE'})
-    console.log(response)
-
-}
+import { useNavigate } from "react-router-dom";
 
 export default function DetailsPage() {
-    return (
-    <div>
-        <Header />
-        {
-        getSingleCard()
+
+    const [data, setData] = useState({});
+    const navigate = useNavigate();
+
+    const deleteTask = async () => {
+        
+        const queryString = document.location.search;
+        const param = new URLSearchParams(queryString);
+        const id = param.get("id")
+        const urlMongoDbdelete = `http://10.0.0.68:5000/delete/${id}`;
+        const response = await fetch(urlMongoDbdelete, {method: 'DELETE'})
+        console.log(response)
+    }
+    
+    const deleteCard = () => {
+        const deleteMessage = {title: "TASK DELETED"}
+        setData(deleteMessage);
+        deleteTask();
+        setTimeout(
+            () => {
+                navigate(`/`);
+            }, 1000
+        );
+    }
+
+    try {
+        const queryString = document.location.search;
+        const param = new URLSearchParams(queryString);
+        const id = param.get("id");
+    
+        const doFetch = async () => {
+            const urlMongoDb = `http://10.0.0.68:5000/task/${id}`;
+            const response = await fetch(urlMongoDb);
+            const json = await response.json();
+    
+            return json;
         }
-        <main>
-            <Link className="margin" to="/"><h2>Back <i className="fa-solid fa-backward-step"></i></h2></Link>
-            <div className="form-container">
-              <TaskForm></TaskForm>
+    
+        const getCard = async () => {
+            let myData = await doFetch()
+            setData(myData);
+        }
+    
+        useEffect(() => {
+            getCard()
+        }, []);
+
+        if(data) {
+            return (
+                <div>
+                    <Header />
+                    <main>
+                        <Link className="margin" to="/"><h2>Back <i className="fa-solid fa-backward-step"></i></h2></Link>
+                        <div className="form-container">
+                          <TaskForm></TaskForm>
+                        </div>
+                        <div className="single-card">
+                            <div className="material-symbols-outlined" onClick={deleteCard}><i className="fa-solid fa-trash"></i></div>
+                            <h1 className="underline">{data.title}</h1>
+                            <p>{data.description}</p>
+                            <p>{data.category}</p>
+                            <p>{data.deadline}</p>
+                            <p>{data.author}</p>
+                        </div> 
+                    </main>
+                </div>
+                );
+        }
+        else {
+            return(
+                <div>
+                    <Header/>
+                    <TaskForm/>
+                    <h1>Card deleted or not found</h1>
+                </div>
+            )
+        }
+
+    } catch(error) {
+        console.log(error);
+        return(
+            <div>
+                <Header />
+                <main>
+                    <h1>Error</h1>
+                </main>
             </div>
-            <div className="single-card">
-                <div className="material-symbols-outlined" onClick={deleteTask}><i className="fa-solid fa-trash"></i></div>
-                <h1 className="underline">{task.title}</h1>
-                <p>Description: {task.description}</p>
-                <p>Category: {task.category}</p>
-                <p>Deadline: {task.deadline}</p>
-                <p>Author: {task.author}</p>
-            </div> 
-        </main>
-    </div>
-    );
+        )
+    }
     }
