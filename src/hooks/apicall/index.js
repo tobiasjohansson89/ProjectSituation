0687d2e3313d945e798b65816e4be9e64dddde45
tobiasjohansson89 <35295187+tobiasjohansson89@users.noreply.github.com>
2023-07-02 15:45:23 +0200

    typo

--- src/hooks/apicall/index.js
+++ src/hooks/apicall/index.js
@@ -1,6 +1,6 @@
 import {useState, useEffect} from "react";
 
-export default function useFetchl(url) {
+export default function useFetch(url) {
 
     const [data, setData] = useState([]);
     const [loading, setLoading] = useState(false);
