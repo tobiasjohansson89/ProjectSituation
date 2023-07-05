0e90beb659f420b38e8974f2a750ea8cfcabb13f
Trine Marie Sandberg <trine.marie.sandberg@gmail.com>
2023-07-05 14:09:12 +0200

    adds user register form

---
+++ src/components/login-form.jsx
@@ -0,0 +1,75 @@
+import React, { useState } from 'react';
+
+const RegisterForm = () => {
+  const [email, setEmail] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [repeatPassword, setRepeatPassword] = useState('');
+  const [passwordError, setPasswordError] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (password !== repeatPassword) {
+      setPasswordError('Passwords do not match');
+      return;
+    }
+
+    // Your registration logic here
+
+    setEmail('');
+    setUsername('');
+    setPassword('');
+    setRepeatPassword('');
+    setPasswordError('');
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Email:
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+      </label>
+      <br />
+      <label>
+        Username:
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          required
+        />
+      </label>
+      <br />
+      <label>
+        Password:
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+      </label>
+      <br />
+      <label>
+        Repeat Password:
+        <input
+          type="password"
+          value={repeatPassword}
+          onChange={(e) => setRepeatPassword(e.target.value)}
+          required
+        />
+      </label>
+      {passwordError && <div style={{ color: 'red' }}>{passwordError}</div>}
+      <br />
+      <button type="submit">Register</button>
+    </form>
+  );
+};
+
+export default RegisterForm;
